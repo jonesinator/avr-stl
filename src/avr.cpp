82a3eb3e999c5df7444bedc570488987d5e69d09
Aaron Jones <aaron@jonesinator.com>
2015-11-01 11:09:30 -0700

    Add more cxa functions to allow for static variable initialization.

--- src/avr.cpp
+++ src/avr.cpp
@@ -1,7 +1,23 @@
 #include <stdlib.h>
 
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+extern "C" int __cxa_guard_acquire(__guard*);
+extern "C" void __cxa_guard_release(__guard*);
+extern "C" void __cxa_guard_abort(__guard*);
 extern "C" void __cxa_pure_virtual(void) __attribute__ ((__noreturn__));
 extern "C" void __cxa_deleted_virtual(void) __attribute__ ((__noreturn__));
+
+int __cxa_guard_acquire(__guard* g) {
+    return !*(char*)(g);
+}
+
+void __cxa_guard_release(__guard* g) {
+    *(char*)g = 1;
+}
+
+void __cxa_guard_abort(__guard* g __attribute__ ((unused))) {
+}
 
 void __cxa_pure_virtual(void) {
     abort();
